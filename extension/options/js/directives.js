5a4677b92f0638e3427b78d85a6292dfea79ff78
taichi <ryushi@gmail.com>
2012-12-12 20:56:20 +0900

    $() を使うのをやめた。

--- extension/options/js/directives.js
+++ extension/options/js/directives.js
@@ -3,17 +3,17 @@ optionsModule.directive('alert', function($compile) {
     restrict : 'A',
     terminal : true,
     compile : function(element, attr) {
+      var template = '<div class="alert fade in {{type}}"><button type="button" class="close" data-dismiss="alert">&times;</button>{{message}}</div>';
       return function(scope, elem) {
         var toClass = function(type) {
           return _.isUndefined(type) ? ''/*alert-warn*/ : 'alert-' + type;
         };
         scope.$on('event:alert', function(event, config) {
-          var newone = $('<div class="alert fade in {{type}}"><button type="button" class="close" data-dismiss="alert">&times;</button>{{message}}</div>');
-          elem.append(newone);
           var child = scope.$new(true);
           child.type = toClass(config.type);
           child.message = config.message;
-          $compile(newone.contents())(child);
+          var newone = $compile(template)(child);
+          elem.append(newone);
           newone.one('close', child, function(event) {
             event.data.$destroy();
           });
