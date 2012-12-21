e4439abaeb533514190fc7a55269fea8f3221f62
taichi <ryushi@gmail.com>
2012-12-21 21:34:19 +0900

    add i18n tests.

---
+++ test/unit/options/i18n.spec.js
@@ -0,0 +1,80 @@
+describe('i18n', function() {
+
+  describe('translate', function() {
+    beforeEach(module('i18n'));
+    var chromeMock;
+    beforeEach(module(function($provide) {
+      $provide.factory('chrome.i18n', function() {
+        chromeMock = jasmine.createSpyObj('chrome.i18n', ['getMessage']);
+        return chromeMock;
+      });
+    }));
+
+    var translate;
+    beforeEach(inject(function($injector) {
+      translate = $injector.get('translate');
+    }));
+
+    it('should work normally.', function() {
+      var args = ['aaa', 'bbb'];
+      translate(args);
+      expect(chromeMock.getMessage).toHaveBeenCalledWith(args);
+    });
+  });
+
+  describe('directives', function() {
+    beforeEach(module('i18n.directives'));
+    var $compile, $scope, translateMock;
+    beforeEach(module(function($provide) {
+      $provide.factory('translate', function() {
+        translateMock = jasmine.createSpy('translate');
+        return translateMock;
+      });
+    }));
+
+    beforeEach(inject(function($injector) {
+      $compile = $injector.get('$compile');
+      $scope = $injector.get('$rootScope');
+    }));
+
+    describe('Element', function() {
+      it('should work normally.', function() {
+        var element = $compile('<translate key="aaa">zzzz</translate>')($scope);
+
+        var returnValue = 'pppp';
+        translateMock.andReturn(returnValue);
+
+        $scope.$digest();
+        expect(element.text()).toEqual(returnValue);
+        expect(translateMock).toHaveBeenCalledWith('aaa');
+      });
+
+      it('fail to translate', function() {
+        var element = $compile('<translate key="aaa">zzzz</translate>')($scope);
+
+        $scope.$digest();
+        expect(element.text()).toEqual('zzzz');
+        expect(translateMock).toHaveBeenCalledWith('aaa');
+      });
+    });
+    describe('attribue', function() {
+      it('should work normally.', function() {
+        var element = $compile('<input placeholder="zzzz" translate="{placeholder:\'aaa\'}">bbb</input>')($scope);
+        
+        var returnValue = 'pppp';
+        translateMock.andReturn(returnValue);
+        
+        $scope.$digest();
+        expect(element.attr('placeholder')).toEqual(returnValue);
+        expect(translateMock).toHaveBeenCalledWith('aaa');
+      });
+      it('fail to translate', function(){
+        var element = $compile('<input placeholder="zzzz" translate="{placeholder:\'aaa\'}">bbb</input>')($scope);
+        
+        $scope.$digest();
+        expect(element.attr('placeholder')).toEqual('zzzz');
+        expect(translateMock).toHaveBeenCalledWith('aaa');
+      });
+    });
+  });
+});
